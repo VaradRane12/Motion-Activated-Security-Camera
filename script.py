1ae073f1f380ab75f65073e94d4610a64d989ccb
VaradRane12 <varadjrane@gmail.com>
2025-05-27 21:47:46 +0530

    changed to picamera2

--- script.py
+++ script.py
@@ -1,18 +1,17 @@
 from gpiozero import MotionSensor
-from picamera import PiCamera
+from picamera2 import Picamera2, Preview
 import time
-from time import sleep
 
 pir = MotionSensor(4)
-camera = PiCamera()
-camera.rotation = 180
+picam2 = Picamera2()
+
+picam2.start_preview(Preview.QTGL)
+picam2.set_controls({"Rotation": 180})
 
 while True:
     pir.wait_for_motion()
     print("Motion detected!")
-    camera.start_preview(fullscreen=False, window = (1250,10,640,480))
-    filename = "/home/pi/Desktop/" + (time.strftime("%y%b%d_%H:%M:%S")) + ".h264"
-    camera.start_recording(filename)
+    filename = "/home/pi/Desktop/" + time.strftime("%y%b%d_%H:%M:%S") + ".mp4"
+    picam2.start_recording(filename)
     pir.wait_for_no_motion()
-    camera.stop_recording()
-    camera.stop_preview()
+    picam2.stop_recording()
