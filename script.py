df21a42db554bf6c566139c0be77a0afbba1fddb
VaradRane12 <varadjrane@gmail.com>
2025-05-27 21:56:14 +0530

    changed to libcamera

--- script.py
+++ script.py
@@ -1,17 +1,29 @@
 from gpiozero import MotionSensor
 from picamera2 import Picamera2, Preview
 import time
+import os
 
 pir = MotionSensor(4)
 picam2 = Picamera2()
 
-picam2.start_preview(Preview.QTGL)
+picam2.configure(picam2.create_video_configuration())
 picam2.set_controls({"Rotation": 180})
+
+# Optional: Comment this out if using headless
+picam2.start_preview(Preview.QT)
 
 while True:
     pir.wait_for_motion()
     print("Motion detected!")
-    filename = "/home/pi/Desktop/" + time.strftime("%y%b%d_%H:%M:%S") + ".mp4"
-    picam2.start_recording(filename)
+    
+    timestamp = time.strftime("%y%b%d_%H:%M:%S")
+    h264_path = f"/home/pi/Desktop/{timestamp}.h264"
+    mp4_path = f"/home/pi/Desktop/{timestamp}.mp4"
+
+    picam2.start_recording(h264_path)
     pir.wait_for_no_motion()
     picam2.stop_recording()
+    
+    # Convert to .mp4 using MP4Box if installed
+    os.system(f"MP4Box -add {h264_path} {mp4_path}")
+    os.remove(h264_path)
