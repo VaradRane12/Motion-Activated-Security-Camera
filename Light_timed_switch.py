b79e0d781a76adf9f888189452ff2eded2a11c1c
VaradRane12 <varadjrane@gmail.com>
2025-06-11 09:42:13 +0530

    added light schedule

---
+++ Light_timed_switch.py
@@ -0,0 +1,20 @@
+import os
+import time
+import schedule
+
+def turn_on_light():
+    os.system('mosquitto_pub -h localhost -t home/light1 -m "ON"')
+    print("Light turned ON at 22:17 AM")
+
+def turn_off_light():
+    os.system('mosquitto_pub -h localhost -t home/light1 -m "OFF"')
+    print("Light turned OFF at 22:22 AM")
+
+schedule.every().day.at("09:45").do(turn_on_light)
+schedule.every().day.at("09:55").do(turn_off_light)
+
+print("MQTT Light Scheduler running...")
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
