d59c97930f58626746c2d3b68c1965b36d04b519
VaradRane12 <varadjrane@gmail.com>
2025-06-17 11:46:53 +0530

    added a init db for making hte database

--- flask/data_adding.py
+++ flask/data_adding.py
@@ -1,7 +1,7 @@
 from app import app, db
-from models import Light
+from models import Device
 
 with app.app_context():
-    light1 = Light(name="parking light", room="parking")
+    light1 = Device(name="parking light", room="parking")
     db.session.add(light1)
     db.session.commit()
