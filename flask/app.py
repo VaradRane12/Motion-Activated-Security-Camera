03963c473ca8c0d40258738774028a26166308dc
VaradRane12 <varadjrane@gmail.com>
2025-06-17 17:03:04 +0530

    test for light swtich

--- flask/app.py
+++ flask/app.py
@@ -37,7 +37,10 @@ s3_client = boto3.client(
 )
 
 def get_video_files():
-    response = s3_client.list_objects_v2(Bucket=S3_BUCKET, Prefix=S3_PREFIX)
+    try:
+        response = s3_client.list_objects_v2(Bucket=S3_BUCKET, Prefix=S3_PREFIX)
+    except:
+        print("cant get to s3")
     videos = []
     print(response["Contents"][-1])
     
