aaaee3f593bedf31a4f9b7f1cc37c1a78a415713
VaradRane12 <varadjrane@gmail.com>
2025-06-17 22:41:44 +0530

    test for light swtich

--- flask/models.py
+++ flask/models.py
@@ -6,6 +6,5 @@ db = SQLAlchemy()
 class Device(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    location = db.Column(db.String(100))
     last_updated = db.Column(db.DateTime, default=datetime.utcnow)
     status = db.Column(db.String(10), default='OFF')  # <--- "ON" or "OFF"
